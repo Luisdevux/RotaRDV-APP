56457ffe532f88e8d3a51b21ceed140238a15f29
Luis Felipe Lopes <luis.felipe.lopes1275@gmail.com>
2026-07-28 18:40:09 -0400

    feat(URL-API): Adicionando url da api para o app consumir

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
   // ATENÇÃO: Se estiver rodando no celular físico, troque localhost (ou 10.0.2.2) 
   // pelo IP da sua máquina na rede local (ex: 192.168.1.15)
-  static const String baseUrl = 'http://127.0.0.1:5050';
+  static const String baseUrl = 'https://rotardv.api.luisfelipe.dpdns.org';
 }
